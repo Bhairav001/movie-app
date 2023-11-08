49e317c2cc7201d294b75fbc54984922954c8425
Bhairav <bhairavgotam066@gmail.com>
2023-11-08 16:48:59 +0530

    added redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useEffect } from "react"
 import { fetchDataFromApi } from "./utils/api"
+import { useDispatch, useSelector } from "react-redux"
+import { getApiConfiguration } from "./store/homeSlice"
 
 
 function App(){
 
+  const dispatch = useDispatch()
+  const {url} = useSelector((store)=>store.home)
+  console.log("url",url)
   useEffect(()=>{
       apiTesting()
   },[])
@@ -11,11 +16,13 @@ function App(){
     fetchDataFromApi("/movie/popular")
     .then((res)=>{
       console.log("res",res)
+      dispatch(getApiConfiguration(res))
     })
   }
   return (
     <>
       App
+      {url?.total_pages}
     </>
   )
 }
