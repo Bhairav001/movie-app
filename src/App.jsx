b172e60649199c5a6c3306cbe61af6e45f4b8337
Bhairav <bhairavgotam066@gmail.com>
2023-11-09 21:40:22 +0530

    added naviation bar

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,19 @@ function App(){
     fetchDataFromApi("/configuration")
     .then((res)=>{
       console.log("res",res)
-      dispatch(getApiConfiguration(res))
+
+      const url ={
+         backdrop:res.images.secure_base_url + "original",
+         poster:res.images.secure_base_url + "original",
+         profile:res.images.secure_base_url + "original"
+
+      }
+      dispatch(getApiConfiguration(url))
     })
   }
   return (
     <BrowserRouter>
-    {/* <Header/> */}
+    <Header/>
        <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/:mediaType/:id" element={<Details/>}/>
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -2,27 +2,44 @@ import React, { useEffect, useState } from 'react'
 import "./style.scss"
 import { useNavigate } from 'react-router-dom';
 import useFetch from '../../../hooks/useFetch';
+import { useSelector } from 'react-redux';
+import Img from '../../../components/lazyLoadImage/Img';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
 
   const navigate = useNavigate();
 
-  const {data, loading}= useFetch("/movie/upcoming");
+  const { data, loading } = useFetch("/movie/upcoming");
 
-  useEffect(()=>{
-    const bg = data?.results?.[Math.floor(Math.random()*20)]?.backdrop_path;
+
+  const { url } = useSelector((state) => state.home);
+
+
+  useEffect(() => {
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
     setBackground(bg)
 
-  },[data])
-  const searchQueryHandler=(event)=>{
-     if(event.key=="Enter"&& query.length>0){
-          navigate(`/search/${query}`)
-     }
+  }, [data])
+
+  const searchQueryHandler = (event) => {
+    if (event.key == "Enter" && query.length > 0) {
+      navigate(`/search/${query}`)
+    }
   }
   return (
     <div className="heroBanner">
-      <div className="wrapper">
+      {!loading && <div className="backdrop-img">
+        <Img src={background} />
+      </div>}
+
+
+     <div className="opacity">
+      
+     </div>
+
+      <ContentWrapper>
         <div className="heroBannerContent">
           <span className="title">Welcome</span>
           <span className="subTitle">
@@ -32,13 +49,13 @@ const HeroBanner = () => {
             <input
               type="text"
               placeholder='Search for a movie or tv show...'
-              onChange={(e)=>setQuery(e.target.value)}
+              onChange={(e) => setQuery(e.target.value)}
               onKeyUp={searchQueryHandler}
             />
             <button>Search</button>
           </div>
         </div>
-      </div>
+      </ContentWrapper>
     </div>
   )
 }
