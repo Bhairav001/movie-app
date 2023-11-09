a72e615a85dd9182a9c884c85dd697e4e540c30c
Bhairav <bhairavgotam066@gmail.com>
2023-11-09 14:41:12 +0530

    added redux and api update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useEffect } from "react"
 import { fetchDataFromApi } from "./utils/api"
 import { useDispatch, useSelector } from "react-redux"
 import { getApiConfiguration } from "./store/homeSlice"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/home/Home"
+import Details from "./pages/details/Details"
+import SearchResult from "./pages/searchResult/SearchResult"
+import Explore from "./pages/explore/Explore"
+import PageNotFound from "./pages/404/PageNotFound"
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
 
 
 function App(){
@@ -10,20 +18,27 @@ function App(){
   const {url} = useSelector((store)=>store.home)
   console.log("url",url)
   useEffect(()=>{
-      apiTesting()
+    fetchApiConfig()
   },[])
-  const apiTesting =()=>{
-    fetchDataFromApi("/movie/popular")
+  const fetchApiConfig =()=>{
+    fetchDataFromApi("/configuration")
     .then((res)=>{
       console.log("res",res)
       dispatch(getApiConfiguration(res))
     })
   }
   return (
-    <>
-      App
-      {url?.total_pages}
-    </>
+    <BrowserRouter>
+    {/* <Header/> */}
+       <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/:mediaType/:id" element={<Details/>}/>
+          <Route path="/search/:query" element={<SearchResult/>}/>
+          <Route path="/explore/:mediaType" element={<Explore/>}/>
+          <Route path="*" element={<PageNotFound/>}/>
+       </Routes>
+       {/* <Footer/> */}
+    </BrowserRouter>
   )
 }
 
