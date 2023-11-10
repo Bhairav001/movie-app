98e846a7d49c974bce6e5241b74066176448c560
Bhairav <bhairavgotam066@gmail.com>
2023-11-10 19:34:32 +0530

    added genres update

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -14,6 +14,7 @@ import PosterFallback from "../../assets/no-poster.png";
 
 import "./style.scss";
 import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
 const Carousel = ({data,loading}) => {
     const carouselContainer = useRef();
 
@@ -59,6 +60,7 @@ const Carousel = ({data,loading}) => {
                             <div className="posterBlock">
                                 <Img src={posterUrl}/>
                                 <CircleRating rating={item.vote_average.toFixed(1)}/>
+                                <Genres data={item.genre_ids.slice(0,2)}/>
                             </div>
                             <div className="textBlock">
                                     <span className="title">
