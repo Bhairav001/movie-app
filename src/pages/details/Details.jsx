32dd589567a53e5282c19658ab67bdfd81f6c4b8
Bhairav <bhairavgotam066@gmail.com>
2023-11-13 09:32:33 +0530

    added Similar and recommandation movies

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -6,6 +6,8 @@ import DetailsBanner from './detailsBanner/DetailsBanner'
 import { useSelector } from 'react-redux'
 import Cast from './cast/Cast'
 import VideosSection from './videosSection/VideosSection'
+import Similar from './carousels/Similar'
+import Recommendation from './carousels/Recommendation'
 const Details = () => {
 
   const {mediaType,id} = useParams();
@@ -20,6 +22,8 @@ const Details = () => {
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
       <Cast data={credits?.cast} loading={creditsLoading}/>
       <VideosSection data={data} loading={loading}/>
+      <Similar mediaType={mediaType} id={id}/>
+      <Recommendation  mediaType={mediaType} id={id}/>
     </div>
   )
 }
