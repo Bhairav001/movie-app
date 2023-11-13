9d23ec71ce831148757c79a727184d65d8fcb6b8
Bhairav <bhairavgotam066@gmail.com>
2023-11-13 09:54:55 +0530

    added search update

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.scss"
+import { useParams } from 'react-router-dom';
+import { fetchDataFromApi } from '../../utils/api';
 const SearchResult = () => {
+  const [data, setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const {query} = useParams();
+
+  const fetchInitialData =()=>{
+    setLoading(true);
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`)
+  }
+
   return (
-    <div>SearchResult</div>
+    <div className='searchResultsPage'>
+              
+    </div>
   )
 }
 
