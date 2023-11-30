98f2721604f61901a01ca7a311fe0f61d340ced4
Bhairav <bhairavgotam066@gmail.com>
2023-11-30 16:03:01 +0530

    added signup and login functionallity

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+// Login.js
+
+import React, { useState } from 'react';
+import '../signup/style.scss';
+import Modal from '../signup/modal/Modal';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+const [registeredUsers, setRegisteredUsers] = useState([])
+  const [modalMessage, setModalMessage] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate()
+  // Retrieve registeredUsers from localStorage, or initialize an empty array
+  const registeredUsers1 = JSON.parse(localStorage.getItem('userData')) || [];
+  registeredUsers.push(registeredUsers1)
 
-export default Login
+//   console.log("c",registeredUsers)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    // Check if any input field is empty
+    if (Object.values(formData).some((value) => value === '')) {
+      setModalMessage('Please enter all details first.');
+      setIsModalOpen(true);
+      return;
+    }
+  
+    // Check if the entered email and password match any registered user
+    let userFound = false;
+  
+    for (let i = 0; i < registeredUsers.length; i++) {
+    //   const user = registeredUsers[i];
+      if (registeredUsers[i].email === formData.email && registeredUsers[i].password === formData.password) {
+        userFound = true;
+        console.log("a",registeredUsers[i].email)
+        console.log("b",formData.email)
+        // break;
+      }
+    }
+  
+    if (userFound){
+      // Reset form data
+      setFormData({
+        email: '',
+        password: '',
+      });
+  
+      // Show success alert
+      setModalMessage('Login successfully!');
+      setTimeout(()=>{
+        navigate("/")
+      },2000)
+    } else {
+      // Show error alert
+      setModalMessage('Something went wrong. Please check your email and password.');
+    }
+  
+    setIsModalOpen(true);
+  };
+  
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  return (
+    <div className="signup-container">
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+      {isModalOpen && <Modal message={modalMessage} onClose={closeModal} />}
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,32 +1,107 @@
 // SignUpForm.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
+import Modal from './modal/Modal';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      });
+    const navigate = useNavigate()
+      const [modalMessage, setModalMessage] = useState('');
+      const [isModalOpen, setIsModalOpen] = useState(false);
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+
+    // Check if any input field is empty
+    if (Object.values(formData).some((value) => value === '')) {
+      setModalMessage('Please enter all details first.');
+      setIsModalOpen(true);
+      return;
+    }
+    
+        // Store data in localStorage
+        localStorage.setItem('userData', JSON.stringify(formData));
+    
+        // Reset form data
+        setFormData({
+          name: '',
+          email: '',
+          password: '',
+          confirmPassword: '',
+        });
+    
+        // Show success alert
+        setModalMessage('Register successfully!');
+        setIsModalOpen(true);
+        setTimeout(()=>{
+            navigate("/login")
+        },2000)
+      };
+    
+      const closeModal = () => {
+        setIsModalOpen(false);
+      };
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
+      };
   return (
     <div className="signup-container">
-      <form className="signup-form">
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" placeholder="Enter your name" />
+    <form className="signup-form" onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Enter your name"
+        value={formData.name}
+        onChange={handleChange}
+      />
 
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="Enter your email" />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Enter your email"
+        value={formData.email}
+        onChange={handleChange}
+      />
 
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="Enter your password" />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Enter your password"
+        value={formData.password}
+        onChange={handleChange}
+      />
 
-        <label htmlFor="confirm-password">Confirm Password</label>
-        <input
-          type="password"
-          id="confirm-password"
-          name="confirm-password"
-          placeholder="Confirm your password"
-        />
+      <label htmlFor="confirm-password">Confirm Password</label>
+      <input
+        type="password"
+        id="confirm-password"
+        name="confirmPassword"
+        placeholder="Confirm your password"
+        value={formData.confirmPassword}
+        onChange={handleChange}
+      />
 
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+      <button type="submit">Sign Up</button>
+    </form>
+    {isModalOpen && (
+        <Modal message={modalMessage} onClose={closeModal} />
+      )}
+  </div>
   );
 };
 
